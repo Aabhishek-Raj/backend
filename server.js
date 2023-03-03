c57bb2e57a7e746cf19ed88a96ee7afba7a18dea
Abhishek R.S <rsabhishek77@gmail.com>
2023-03-03 17:56:56 +0530

    updated

--- server.js
+++ server.js
@@ -38,20 +38,17 @@ app.use('/chats', require('./routes/chatRoutes'))
 app.use('/order', require('./routes/orderRoutes'))
 
 
-// app.all('*', (req, res) => {
-//     res.status(404)
-//     if(req.accepts('html')){
-//         res.sendFile(path.join(__dirname, 'views', '404.html'))
-//     } else if(req.accepts('json')){
-//         res.json({message: '404 Not found'}) 
-//     }else{
-//         res.type('txt').send('404 Not found')
-//     }
-// })
-
-app.listen('*', () => {
-    //send the index.html from buid folder
+app.all('*', (req, res) => {
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if(req.accepts('json')){
+        res.json({message: '404 Not found'}) 
+    }else{
+        res.type('txt').send('404 Not found')
+    }
 })
+
 
 app.use(errorHandler)
 
